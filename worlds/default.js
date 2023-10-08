9302ee73d1a627e9a075ab20c3a04bf17766766f
Kafter <115289409+inf2021090@users.noreply.github.com>
2023-10-08 20:05:35 +0300

    Update default.js

--- worlds/default.js
+++ worlds/default.js
@@ -1,44 +1,36 @@
+// tutorial2.js
 // Copyright 2022 by Croquet Corporation, Inc. All Rights Reserved.
 // https://croquet.io
 // info@croquet.io
 
 export function init(Constants) {
-    Constants.AvatarNames = ["newwhite"];
-
-    /* Alternatively, you can specify a card spec for an avatar,
-       instead of a string for the partical file name, to create your own avatar.
-       You can add behaviorModules here. Also, if the system detects a behavior module
-       named AvatarEventHandler, that is automatically installed to the avatar.
-        {
-            type: "3d",
-            modelType: "glb",
-            name: "rabbit",
-            dataLocation: "./assets/avatars/newwhite.zip",
-            dataRotation: [0, Math.PI, 0],
-            dataScale: [0.3, 0.3, 0.3],
-        }
-    */
-
-    Constants.UserBehaviorDirectory = "behaviors/default";
-    Constants.UserBehaviorModules = [
-        "lights.js"
+    Constants.AvatarNames = [
+        "newwhite", "madhatter", "marchhare", "queenofhearts", "cheshirecat", "alice"
     ];
+
+    Constants.UserBehaviorDirectory = "behaviors/tutorial";
+    Constants.UserBehaviorModules = [
+        "lights.js", "joeTheBox.js", "simpleSpin.js"
+    ];
+
+    // const frameColor = 0x888888;
 
     Constants.DefaultCards = [
         {
             card: {
                 name:"world model",
-                layers: ["walk"],
                 type: "3d",
-                fileName: "/art_floor1.glb",
+                dataLocation: "./assets/3D/artgallery_042122.glb.zip",
+                dataScale:[1,1,1],
                 singleSided: true,
                 shadow: true,
+                layers: ["walk"],
                 translation:[0, -1.7, 0],
-                dataScale:[1,1,1],
+
                 placeholder: true,
-                placeholderSize: [400, 0.1, 400],
-                placeholderColor: 0xe0e0e0,
-                placeholderOffset: [0, 0, 0],
+                placeholderSize: [100, 0.01, 100],
+                placeholderColor: 0xcccccc,
+                placeholderOffset: [0, -1.7, 0],
             }
         },
         {
@@ -47,11 +39,50 @@ export function init(Constants) {
                 layers: ["light"],
                 type: "lighting",
                 behaviorModules: ["Light"],
-                dataLocation: "3OF2-s4U1ZOJduGATmLEIXo1iTkQHd5ZBknKgL5SvqpQJzs7Pzx1YGApJiMqPGE6PGEsPSA-Oio7YSYgYDpgCCsZLTYjBjwOJB4sDRcrfAg3Ljk2OBoEGBYWfWAmIGEsPSA-Oio7YSImLD0gOSo9PCpgPwB9AAIIISx8YiYneScqKyQaIisNLHkaGT8YKg56JQwQfHstPiNiGQ49e2ArLjsuYCMBPgMiCQt3OQskGhcleSp9HQIIfXseHgo7EAo9CB48FRwpegsCLH4OIwY",
-                fileName: "/abandoned_parking_4k.jpg",
-                dataType: "jpg",
-                toneMappingExposure: 1.2
+                dataLocation: "./assets/sky/shanghai_riverside_2k.exr",
+                dataType: "exr",
+                toneMappingExposure: 1.2,
             }
-        }
+        },
+        {
+            card: {
+                name: "image card",
+                translation: [0, 0.4, -10],
+                //rotation: [0, Math.PI / 2, 0],
+                scale: [4, 4, 4],
+                type: "2d",
+                textureType: "image",
+                textureLocation: "./assets/images/CroquetLogo_RGB.jpg",
+                fullBright: true,
+                frameColor: 0xcccccc,
+                color: 0xbbbbbb,
+                cornerRadius: 0.05,
+                depth: 0.05,
+                shadow: true,
+            }
+        },
+        {
+            card: {
+                name:"Joe the Box",
+                behaviorModules: ["JoeTheBox", "SimpleSpin"],
+                layers: ["pointer"],
+                type: "object",
+                translation:[-4, 0.4, -10],
+                shadow: true,
+            }
+        },
+        {
+            card: {
+                name:"Imported Box",
+                type: "3d",
+                dataLocation: "3huoVKegegBeYdI78lg1H2JMKp5sGwGU8aEoR1uckNZcABwcGBtSR0cOAQQNG0YdG0YLGgcZHQ0cRgEHRx1HLww-ChEEIRspAzkLKjAMWy8QCR4RHz0jPzExWkcBB0YLGgcZHQ0cRgUBCxoHHg0aGw1HWiUAISwyPw0CAQsPPFkZXwwnEl4sHQM6MAElKllFAQIjKyxaJjEMBSMKIUcMCRwJRwYYP1ARGwwtPj0lUDELLAsbJQBbAAE9CQc-IB4qDFxdCxhQCxgbERIBPxs",
+                fileName: "/testcube_1m.glb.zip",
+                layers: ["pointer"],
+                translation:[4, 0.4, -10],
+                dataScale:[1,1,1],
+                //rotation:[0, Math.pi/4, 0],
+                shadow: true,
+            }
+        },
     ];
 }
