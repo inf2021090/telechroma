5287e79b84c42402db8d27600755add2ba5f2d84
inf2021090 <inf2021090@ionio.gr>
2023-10-09 09:22:34 +0000

    updated dev key

--- apiKey-dev.js
+++ apiKey-dev.js
@@ -1,4 +1,4 @@
- /* Copy this into a file named apiKey.js */
+ /* you can use your own key from croquet.io/keys for development */
 const apiKey = '1jnrlm4OItxhjL7eF2dm8NK0srzmMhhjEq7fIgzon';
 const appId = 'com.gmail.gaskaf124.microverse';
 export default  {apiKey, appId};
