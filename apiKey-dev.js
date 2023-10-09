2b0885a00eeb41dc4062a64391e4cae7c4ec0ab0
inf2021090 <inf2021090@ionio.gr>
2023-10-09 09:02:38 +0000

    updated world

---
+++ apiKey-dev.js
@@ -0,0 +1,12 @@
+ /* Copy this into a file named apiKey.js */
+const apiKey = '1jnrlm4OItxhjL7eF2dm8NK0srzmMhhjEq7fIgzon';
+const appId = 'com.gmail.gaskaf124.microverse';
+export default  {apiKey, appId};
+
+
+
+
+
+
+
+
