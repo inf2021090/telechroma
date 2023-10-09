bb2f57f2997768198198598659bc8cf597773506
Kafter <115289409+inf2021090@users.noreply.github.com>
2023-10-09 12:04:11 +0300

    Update lights.js

--- behaviors/default/lights.js
+++ behaviors/default/lights.js
@@ -1,9 +1,18 @@
-class LightPawn {
+// the following import statement is solely for the type checking and
+// autocompletion features in IDE.  A Behavior cannot inherit from
+// another behavior or a base class but can use the methods and
+// properties of the card to which it is installed.
+// The prototype classes ActorBehavior and PawnBehavior provide
+// the features defined at the card object.
+
+import {ActorBehavior, PawnBehavior} from "../PrototypeBehavior";
+
+class LightPawn extends PawnBehavior {
     setup() {
         let trm = this.service("ThreeRenderManager");
-        let scene =  trm.scene;
-        let camera = trm.camera;
         let group = this.shape;
+        let THREE = Microverse.THREE;
+        // window.myRenderer = trm;
 
         if (this.actor._cardData.toneMappingExposure !== undefined) {
             trm.renderer.toneMappingExposure = this.actor._cardData.toneMappingExposure;
@@ -12,11 +21,35 @@ class LightPawn {
         this.removeLights();
         this.lights = [];
 
-        this.setupCSM(scene, camera, Microverse.THREE);
-
-        const ambient = new Microverse.THREE.AmbientLight( 0xffffff, .7 );
+        const ambient = new THREE.AmbientLight( 0xffffff, .5 );
         group.add(ambient);
         this.lights.push(ambient);
+
+        const sun = new THREE.DirectionalLight( 0xffffff, 0.3 );
+        sun.position.set(9, 150, -10);
+        sun.castShadow = true;
+        sun.shadow.blurSamples = 5;
+        sun.shadow.camera.left = 40;
+        sun.shadow.camera.right = -30;
+        sun.shadow.camera.top = 30;
+        sun.shadow.camera.bottom = -30;
+        sun.shadow.mapSize.width = 2048; // default
+        sun.shadow.mapSize.height = 2048; // default
+        sun.shadow.normalBias = 1e-2;
+        sun.shadow.bias = - 1e-3;
+        sun.shadow.radius = 4;
+        group.add(sun);
+        this.lights.push(sun);
+
+        const blueLight = new THREE.DirectionalLight(0x444488, 0.5);
+        blueLight.position.set(1, 100, 150);
+        group.add(blueLight);
+        this.lights.push(blueLight);
+
+        const redLight = new THREE.DirectionalLight(0x774444, 0.5);
+        redLight.position.set(1, 100, -150);
+        group.add(redLight);
+        this.lights.push(redLight);
 
         this.constructBackground(this.actor._cardData);
 
@@ -29,13 +62,18 @@ class LightPawn {
     removeLights() {
         if (this.lights) {
             [...this.lights].forEach((light) => {
-                light.dispose();
+                if (light.dispose) {
+                    light.dispose();
+                }
                 this.shape.remove(light);
             });
         }
         delete this.lights;
 
         if (this.csm) {
+	    for ( let i = 0; i < this.csm.lights.length; i ++ ) {
+	        this.csm.parent.remove( this.csm.lights[ i ].target );
+	    }
             this.csm.remove();
             this.csm.dispose();
             delete this.csm;
@@ -44,8 +82,13 @@ class LightPawn {
 
     teardown() {
         console.log("teardown lights");
-        if(this.background)this.background.dispose();
         this.removeLights();
+        let scene = this.service("ThreeRenderManager").scene;
+        scene.background?.dispose();
+        scene.environment?.dispose();
+        scene.background = null;
+        scene.environment = null;
+
     }
 
     updateShape(options) {
@@ -53,24 +96,19 @@ class LightPawn {
     }
 
     constructBackground(options) {
-        console.log('lights', options)
         let assetManager = this.service("AssetManager").assetManager;
         let dataType = options.dataType;
-        let TRM = this.service("ThreeRenderManager");
-        let renderer = TRM.renderer;
-        let scene = TRM.scene;
-
-        if (!options.dataLocation) {
-            if (options.clearColor) renderer.setClearColor(options.clearColor);
-            return;
-        }
+        if (!options.dataLocation) {return;}
         return this.getBuffer(options.dataLocation).then((buffer) => {
             return assetManager.load(buffer, dataType, Microverse.THREE, options).then((texture) => {
+                let TRM = this.service("ThreeRenderManager");
+                let renderer = TRM.renderer;
+                let scene = TRM.scene;
                 let pmremGenerator = new Microverse.THREE.PMREMGenerator(renderer);
                 pmremGenerator.compileEquirectangularShader();
 
                 // we treat the color space of the loaded exr texture.
-                texture.colorSpace = Microverse.THREE.SRGBColorSpace;
+                // texture.colorSpace = THREE.SRGBColorSpace;
 
                 let exrCubeRenderTarget = pmremGenerator.fromEquirectangular(texture);
                 let exrBackground = exrCubeRenderTarget.texture;
@@ -87,32 +125,13 @@ class LightPawn {
                 if(e !== bg) if(bg) bg.dispose();
                 if(e) e.dispose();
                 texture.dispose();
+            }).then(() => {
+                if (this.actor._cardData.loadSynchronously) {
+                    this.publish(
+                        this.sessionId, "synchronousCardLoaded", {id: this.actor.id});
+                }
             });
         });
-    }
-
-    setupCSM(scene, camera, THREE) {
-        if (this.csm) {
-            this.csm.remove();
-            this.csm = null;
-        }
-
-        let dir = new THREE.Vector3(-200, -500, -200);
-        this.csm = new THREE.CSM({
-            fade: true,
-            far: camera.far,
-            maxFar: 1000,
-            cascades: 3,
-            shadowMapSize: 2048,
-            shadowbias: 0.00025,
-            lightDirection: dir,
-            camera: camera,
-            parent: scene,
-            lightIntensity: 0.4,
-            lightFar: 1000,
-            mode: "practical"
-        });
-        this.csm.update();
     }
 
     update(_time) {
@@ -126,10 +145,6 @@ export default {
             name: "Light",
             pawnBehaviors: [LightPawn]
         }
-    ]
-}
-
-/* globals Microverse */
     ]
 }
 
